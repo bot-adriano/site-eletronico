c1466c1dde8be7640e3a2891863bd5a16716b337
bot-adriano <89438737+bot-adriano@users.noreply.github.com>
2021-09-29 01:46:40 -0300

    neew

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$('.slider-principal').slick();  //pegamos o modelo tb daquele site de js e como é ". "antes de single teremos que atribuir uma classe etrocaremos  por slider principal que é a clasee que criamos
+//$('.slider-principal').slick();  //pegamos o modelo tb daquele site de js e como é ". "antes de single teremos que atribuir uma classe etrocaremos  por slider principal que é a clasee que criamos
+$('.slider-principal').slick({
+    dots:true,
+    infinite:true,
+    speed:300,
+    slideToShow:1,
+    autoplay: true,
+    autoplaySpeed:2000
+});
